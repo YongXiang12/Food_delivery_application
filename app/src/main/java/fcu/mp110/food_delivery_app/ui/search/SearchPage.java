39bc78ae1905f55d9cf2bce52a4d3cf531a79e8b
xiang <jk660716@gmail.com>
2022-05-01 23:40:40 +0800

    add Menu Search UI and modify Search Page

--- app/src/main/java/fcu/mp110/food_delivery_app/ui/search/SearchPage.java
+++ app/src/main/java/fcu/mp110/food_delivery_app/ui/search/SearchPage.java
@@ -2,7 +2,11 @@ package fcu.mp110.food_delivery_app.ui.search;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import fcu.mp110.food_delivery_app.MainActivity;
 import fcu.mp110.food_delivery_app.R;
 
 public class SearchPage extends AppCompatActivity {
@@ -12,4 +16,10 @@ public class SearchPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.searchpage);
     }
+
+    public void search(View view){
+        Intent it = new Intent(this, SearchResultPage.class);
+        startActivity(it);
+    }
+
 }
