fb1269c0f833d73dba9c58bd0448aebf9f43ee12
maxchenzzz <asdfgtwio123@gmail.com>
2022-05-02 18:28:01 +0800

    merge 側邊選單點擊login跳轉頁面到login ui

---
+++ app/src/main/java/fcu/mp110/food_delivery_app/ui/login/Fragment2.java
@@ -0,0 +1,22 @@
+package fcu.mp110.food_delivery_app.ui.login;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fcu.mp110.food_delivery_app.R;
+
+
+public class Fragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_2, container, false);
+    }
+}
--- app/src/main/java/fcu/mp110/food_delivery_app/ui/login/LoginActivity.java
+++ app/src/main/java/fcu/mp110/food_delivery_app/ui/login/LoginActivity.java
@@ -1,16 +1,34 @@
 package fcu.mp110.food_delivery_app.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 import fcu.mp110.food_delivery_app.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tabLayout = findViewById(R.id.tablayout);
+        viewPager = findViewById(R.id.viewpager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new Fragment1(), "Login");
+        vpAdapter.addFragment(new Fragment2(), "Register");
+
+        viewPager.setAdapter(vpAdapter);
     }
 }
