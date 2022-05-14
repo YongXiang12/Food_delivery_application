9035740cff941d826d412f6d200176e24edd4f99
samel <samel0731>
2022-05-14 21:22:39 +0800

    ReviewActivity介面設計

---
+++ app/src/main/java/fcu/mp110/food_delivery_app/ui/restaurant/ReviewActivity.java
@@ -0,0 +1,16 @@
+package fcu.mp110.food_delivery_app.ui.restaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import fcu.mp110.food_delivery_app.R;
+
+public class ReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+    }
+}
