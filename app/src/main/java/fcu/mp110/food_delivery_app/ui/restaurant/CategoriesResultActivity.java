0e4846751b323eba70d81d0abb80a991e8872e4c
D0852825 <D0852825>
2022-06-08 19:12:29 +0800

    讀餐廳bug

--- app/src/main/java/fcu/mp110/food_delivery_app/ui/restaurant/CategoriesResultActivity.java
+++ app/src/main/java/fcu/mp110/food_delivery_app/ui/restaurant/CategoriesResultActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -30,6 +31,8 @@ public class CategoriesResultActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         categoriesLabel = intent.getStringExtra("categoriesLabel");
+        TextView textView = findViewById(R.id.categoriesResult);
+        textView.setText(categoriesLabel);
         initCategoriesRestaurant();
     }
     public void goBack(View view)
